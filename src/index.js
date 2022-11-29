354f79d4d304758d546e36f900cc8cb8acc603e6
Bondaruk-Vasul <bondaruk.vasyl01@gmail.com>
2022-11-29 15:20:24 +0200

    your message here

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import './style.css';
 import { showPage } from './pageHandler';
 
 const PAGES = {
-  LIST: 'pageList',
-  CREATE: 'pageCreate',
-  EDIT: 'pageEdit',
+    LIST: 'pageList',
+    CREATE: 'pageCreate',
+    EDIT: 'pageEdit',
 };
 
 const UI = {
-  goToCreatePageButton: document.getElementById('go-to-create'),
-  closeCreatePageButton: document.getElementById('create-close-button'),
+    goToCreatePageButton: document.getElementById('go-to-create'),
+    closeCreatePageButton: document.getElementById('create-close-button'),
 };
 
 UI.goToCreatePageButton.addEventListener('click', () => {
-  showPage(PAGES.CREATE);
+    showPage(PAGES.CREATE);
 });
 
 UI.closeCreatePageButton.addEventListener('click', () => {
-  showPage(PAGES.LIST);
+    showPage(PAGES.LIST);
 });
